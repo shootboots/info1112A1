68e590e6c57854f9792ec40c7d5688f6285d5c59
Alex Xu <alexxu@Alexs-Macbook-Pro-2.local>
2026-09-22 12:45:00 +1000

    cmd line arguments

--- assembler.sh
+++ assembler.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+
+if [[ $# -eq 0 ]]; then
+    echo "usage: no argument is provided"
+    exit 1
+fi
+
+if [[ $# -gt 1 ]]; then
+    echo "usage: more than one arguments are provided"
+    exit 1
+fi 
+
+input="$1"
